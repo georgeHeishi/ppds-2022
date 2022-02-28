1577f3f1d4873df7f1fcd60f3363c7cb4447ef1d
Juraj Lapčák <lapcakjuraj@gmail.com>
2022-02-28 23:51:53 +0100

    refactor: move SimpleBarrier class to separate module

--- problem1.py
+++ problem1.py
@@ -1,55 +1,11 @@
 from random import randint
 from time import sleep
-from fei.ppds import Thread, Event, Mutex, print
+from fei.ppds import Thread, print
+from simplebarrier import SimpleBarrier
 
 """
-This program implements class ADT SimpleBarrier and tests it on 100 threads.
+This program uses class ADT SimpleBarrier from simplebarrier.py and tests it on 100 threads.
 """
-
-
-class SimpleBarrier:
-    """
-    Barrier class.
-
-    Attributes:
-    n -- number of threads to block with barrier
-    counter -- number of threads currently blocked
-    mutex -- shared lock that sleeps the program and makes sure concurtent programs runs paralel
-    event -- 
-
-    Methods:
-    wait() -- representation of barrier's turnstile
-    """
-
-    def __init__(self, n):
-        """Constructs attributes for SimpleBarrier object.
-
-        Parameters:
-        n -- number of threads the barrier blocks
-        """
-        self.n = n
-        self.counter = 0
-        self.mutex = Mutex()
-        self.event = Event()
-
-    def wait(self):
-        """
-        Lock's program until all threads completed turnstile.
-
-        Parameters:
-        None
-
-        Returns:
-        None
-        """
-        self.event.clear()
-        self.mutex.lock()
-        self.counter += 1
-        if self.counter == self.n:
-            self.c = 0
-            self.event.signal()
-        self.mutex.unlock()
-        self.event.wait()
 
 
 def barrier_example(barrier, thread_id):
---
+++ simplebarrier.py
@@ -0,0 +1,50 @@
+from fei.ppds import Event, Mutex
+
+"""
+This module implements ADT SimpleBarrier class.
+"""
+
+
+class SimpleBarrier:
+    """
+    Barrier class.
+
+    Attributes:
+    n -- number of threads to block with barrier
+    counter -- number of threads currently blocked
+    mutex -- shared lock that sleeps the program and makes sure concurtent programs runs paralel
+    event -- 
+
+    Methods:
+    wait() -- representation of barrier's turnstile
+    """
+
+    def __init__(self, n):
+        """Constructs attributes for SimpleBarrier object.
+
+        Parameters:
+        n -- number of threads the barrier blocks
+        """
+        self.n = n
+        self.counter = 0
+        self.mutex = Mutex()
+        self.event = Event()
+
+    def wait(self):
+        """
+        Lock's program until all threads completed turnstile.
+
+        Parameters:
+        None
+
+        Returns:
+        None
+        """
+        self.event.clear()
+        self.mutex.lock()
+        self.counter += 1
+        if self.counter == self.n:
+            self.counter = 0
+            self.event.signal()
+        self.mutex.unlock()
+        self.event.wait()
